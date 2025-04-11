4d2fce42d6084846f10dd87ffe047c293d3ddc49
Vhon Angelico C. Salilo <vhonsalilo1@gmail.com>
2025-04-12 02:34:59 +0800

    added authentication

--- app/Traits/ConsumesExternalService.php
+++ app/Traits/ConsumesExternalService.php
@@ -2,23 +2,27 @@
 
 namespace App\Traits;
 
+// Include the Guzzle Component Library
 use GuzzleHttp\Client;
 
 trait ConsumesExternalService
 {
     public function performRequest($method, $requestUrl, $form_params = [], $headers = [])
     {
-        $client = new Client([
-            'base_uri' => $this->baseUri,
-        ]);
+    $client = new Client(['base_uri' => $this->baseUri]);
 
-
-        if (isset($this->secret)) {
-            $headers['Authorization'] = $this->secret;
-        }
-        
-        $response = $client->request($method, $requestUrl, ['form_params' => $form_params, 'headers' => $headers]);
-
-        return $response->getBody()->getContents();
+    
+    if (isset($this->secret)) {
+        $headers['Authorization'] = $this->secret;
     }
+
+    $response = $client->request($method, $requestUrl, [
+        'form_params' => $form_params,
+        'headers' => $headers,
+    ]);
+
+    // Decode JSON before returning
+    return json_decode($response->getBody()->getContents(), true);
+}
+
 }
